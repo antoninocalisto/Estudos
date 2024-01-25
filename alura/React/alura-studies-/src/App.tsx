2ae46655999e159a6b9ca8d0362e67b6d4628508
Antonino Calisto dos Santos Neto <antonino.calisto@wf5.com.br>
2024-01-25 05:43:31 -0300

    feat: inserindo lista em react studies

--- alura/React/alura-studies-/src/App.tsx
+++ alura/React/alura-studies-/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Formulario from './components/Formulario';
+import List from './components/List';
 
 function App() {
   return (
     <div className="App">
      <Formulario />
+     <List />
     </div>
   );
 }
